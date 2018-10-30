72601069feaea51cea002f3784ec63db5ff7c77b
Evgeniy Kazakov <evgeniy@i2x.ai>
2018-10-30 17:09:16 +0100

    wip

--- stories/index.js
+++ stories/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { Button } from '@storybook/react/demo';
+import { Row, Column, Block, FixedBlock100 } from '../src';
+// import { action } from '@storybook/addon-actions';
+// import { Button } from '@storybook/react/demo';
 
-storiesOf('Button', module)
-    .add('with text', () => (
-        <Button onClick={action('clicked')}>Hello Button</Button>
-))
-.add('with some emoji', () => (
-    <Button onClick={action('clicked')}><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
-));
+const stories = storiesOf('Button', module);
+stories.add('One', () => {
+    return <div>
+        <Row>
+            <FixedBlock100>1</FixedBlock100>
+            <Column>
+                <FixedBlock100>1</FixedBlock100>
+                <Row justify="center" style={{ width: 300 }}>
+                    <FixedBlock100>5</FixedBlock100>
+                    <FixedBlock100>6</FixedBlock100>
+                </Row>
+                <FixedBlock100>2</FixedBlock100>
+            </Column>
+            <FixedBlock100>2</FixedBlock100>
+            <FixedBlock100>3</FixedBlock100>
+        </Row>
+    </div>
+});
+
+stories.add('Two', () => {
+    return <div>
+        <Row>
+            <FixedBlock100>1</FixedBlock100>
+            <Column>
+                <FixedBlock100>1</FixedBlock100>
+                <Row justify="center" style={{ width: 300 }}>
+                    <FixedBlock100>5</FixedBlock100>
+                    <FixedBlock100>6</FixedBlock100>
+                </Row>
+                <FixedBlock100>2</FixedBlock100>
+            </Column>
+            <FixedBlock100>2</FixedBlock100>
+            <FixedBlock100>3</FixedBlock100>
+        </Row>
+    </div>
+});
