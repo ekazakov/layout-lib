8e277b58e9900e24287ed4eb3c4decf4aabf20d2
Evgeniy Kazakov <evgeniy@i2x.ai>
2018-10-30 16:30:56 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -7,16 +7,24 @@ export const Block = styled('div')`
 `;
 
 export const Row = styled('div')`
-    ${Block};
+    flex-basis: ${p => p.basis || 'auto'};
+    flex-grow: ${p => p.grow || 0};
     display: flex;
     flex-direction: row;
-    
+    justify-content: ${p => p.justify || 'flex-start'};
 `;
 
 export const Column = styled('div')`
-    ${Block};
+    flex-basis: ${p => p.basis || 'auto'};
+    flex-grow: ${p => p.grow || 0};
     display: flex;
     flex-direction: column;
 `;
 
-export const TextBlock = styled('div')``;
+export const FixedBlock100 = styled('div')`
+  width: 100px;
+  height: 50px;
+  background-color: cornflowerblue;
+  color: white;
+  margin: 5px;
+`;
